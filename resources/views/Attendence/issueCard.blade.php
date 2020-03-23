1d659afd61f3adb6d4e90a5717a9e1c4a6364088
LahiruNikolin <nikolinlahiru@gmail.com>
2020-03-23 11:44:12 +0530

    lancer

--- resources/views/Attendence/issueCard.blade.php
+++ resources/views/Attendence/issueCard.blade.php
@@ -6,8 +6,8 @@
     <div class="spinner-grow text-primary" style="width:10rem; height:10rem;" role="status" id="loader">
         <span class="sr-only">Loading...</span>
     </div>
-    <div     class="card" style="width: 40rem; height:18rem; background-color:#f5f6fa;" id="printJS-card" >
-        <div class="card-body" style="width: 40rem; "  >
+    <div  class="card" style="width: 35rem; height:18rem; background-color:#f5f6fa;" id="printJS-card" >
+        <div class="card-body" style="width: 35rem; "  >
             <div style="background-color:#0984e3; color:white;" class="p-1">
                 <h5 class="card-title">Excellent Institute</h5>
                 <h6 class="card-subtitle mb-1 font-weight-normal">Class card</h6>
